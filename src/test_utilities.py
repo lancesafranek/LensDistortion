39b7a6f8f720032a46d87a1859e7d8c4cfc63903
stefan safranek <stefan@stefan.stefan>
2019-08-10 21:56:10 -0700

    unittests added for utilities.py

--- src/test_utilities.py
+++ src/test_utilities.py
@@ -14,7 +14,7 @@ import utilities
 
 class TestUtilities(unittest.TestCase):
 
-    def test_build_camera_matrices(self):
+    def test_build_camera_matrices_basic(self):
         # expected resultDistortionParameters
         expectedCameraMatrix = '[[12.0, 0.0, 50.0], [0.0, 12.0, 50.0], [0.0, 0.0, 1.0]]'
         expectedDistortionParameters = '[[-0.001], [0.001], [-0.001], [-0.001]]'
@@ -33,8 +33,37 @@ class TestUtilities(unittest.TestCase):
 
         # check values of results
         self.assertTrue(expectedCameraMatrix == json.dumps(resultCameraMatrix.tolist()))
-        self.assertTrue( expectedDistortionParameters == json.dumps(resultDistortionParameters.tolist()) )
+        self.assertTrue(expectedDistortionParameters == json.dumps(resultDistortionParameters.tolist()))
+
+
+    def test_generate_grid_points(self):
+        print(utilities.generate_grid_points(11, 11, 100, 100))
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
+
+gridStartPoints = []
+gridEndPoints = []
+
+num = 11
+res = 11
+w = 100
+h= 100
+xVals = numpy.linspace(0, w, num)
+yVals = numpy.linspace(0, h, num)
+
+stepsW = w/res
+stepsH = h/res
+for i in range(len(xVals)):
+    for j in range(len(yVals)):
+        gridStartPoints.append((xVals[j], yVals[j]))
+
+
+
+
+
+# xv, yv = numpy.meshgrid(x, y)
